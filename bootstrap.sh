67f674e88e13dccddb2d7e0b4189d5f636897f90
Xiao-Ou Zhang <kepbod@gmail.com>
2016-04-20 12:45:08 +0800

    remove mouse mode in tmux.conf

--- bootstrap.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# Author: Xiao-Ou Zhang <kepbod@gmail.com>
-
-# set tmux
-if [ -f $HOME/.tmux.conf ]; then mv $HOME/.tmux.conf $HOME/.tmux.conf.backup; fi
-rm -f $HOME/.tmux.conf
-git clone https://github.com/kepbod/itmux.git $HOME/itmux
-ln -s $HOME/itmux/tmux.conf $HOME/.tmux.conf
